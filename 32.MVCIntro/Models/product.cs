62ac142bc9e09043c220cba3a82122e589ce471a
Roya Rustamova <royaer-bpa203@code.edu.az>
2025-12-11 00:39:13 +0400

    commit

---
+++ 32.MVCIntro/Models/product.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCIntro.Models
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ProductId { get; set; }
+
+    }
+}
